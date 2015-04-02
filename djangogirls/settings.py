46f8c3da9db3818c3f35d9de1a61dee3056b9db2
flaviobarros <flaviomargarito@gmail.com>
2015-04-02 16:37:57 -0300

    Included DATABASES again

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -56,6 +56,12 @@ WSGI_APPLICATION = 'djangogirls.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
