1c0ae7652a3752cf3f419d953e3e5ee79ee1176a
flaviobarros <flaviomargarito@gmail.com>
2015-04-08 11:26:57 -0300

    Incluidos botões para publicar e editar

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 import django.shortcuts
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 from .models import Post
 from .forms import PostForm
@@ -10,11 +11,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return django.shortcuts.render(request, 'blog/post_list.html', {'posts': posts})
 
-
 def post_detail(request, pk):
     post = django.shortcuts.get_object_or_404(Post, pk=pk)
     return django.shortcuts.render(request, 'blog/post_detail.html', {'post': post})
 
+@login_required
 def post_new(request):
 
     if request.method=="POST":
@@ -29,6 +30,7 @@ def post_new(request):
 
     return django.shortcuts.render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
 
     post = django.shortcuts.get_object_or_404(Post, pk=pk)
@@ -43,4 +45,19 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
     return django.shortcuts.render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return django.shortcuts.render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
+def post_publish(request, pk):
+    post = django.shortcuts.get_object_or_404(Post, pk=pk)
+    post.publish()
+    return django.shortcuts.redirect('blog.views.post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = django.shortcuts.get_object_or_404(Post, pk=pk)
+    post.delete()
+    return django.shortcuts.redirect('blog.views.post_list')
